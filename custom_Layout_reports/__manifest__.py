4d296dc58bea90f946931cf0c1756a68af691c11
arslan <arslanyasin786>
2025-08-08 10:59:12 +0500

    [ADD]added the custom layout report

---
+++ custom_Layout_reports/__manifest__.py
@@ -0,0 +1,15 @@
+    # -*- coding: utf-8 -*-
+{
+    'name': "Custom Layout Reports",
+    'summary': "",
+    'description': """""",
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base',"sale","account"],
+    'data': [
+        'views/templates.xml',
+    ],
+}
+
