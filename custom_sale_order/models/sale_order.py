4f9deccd078c5db2ac87ac041917540937e3fd2b
Arsalanyasin786 <arsalanyasin786@gmail.com>
2025-09-16 19:14:32 +0500

    [ADD] Set Provider For Invoice

--- custom_sale_order/models/sale_order.py
+++ custom_sale_order/models/sale_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta
 
-from odoo import _,api, fields, models
+from odoo import _,api, fields, models, Command, _
 from odoo.exceptions import UserError, ValidationError
 import logging
 _logger = logging.getLogger(__name__)
@@ -209,6 +209,48 @@ class SaleOrder(models.Model):
                 }
             )
             _logger.info('Swapped Partner_Id --> %s  Shipping Id --> %s', record.partner_id.display_name,record.partner_shipping_id.display_name)
+    
+        # INVOICING #
+        # To Create invoice for the Provider Field
+    def _prepare_invoice(self):
+        """
+        Prepare the dict of values to create the new invoice for a sales order. This method may be
+        overridden to implement custom invoice generation (making sure to call super() to establish
+        a clean extension chain).
+        """
+        self.ensure_one()
+
+        txs_to_be_linked = self.transaction_ids.sudo().filtered(
+            lambda tx: (
+                tx.state in ('pending', 'authorized')
+                or tx.state == 'done' and not (tx.payment_id and tx.payment_id.is_reconciled)
+            )
+        )
+
+        values = {
+            'ref': self.client_order_ref or '',
+            'move_type': 'out_invoice',
+            'narration': self.note,
+            'currency_id': self.currency_id.id,
+            'campaign_id': self.campaign_id.id,
+            'medium_id': self.medium_id.id,
+            'source_id': self.source_id.id,
+            'team_id': self.team_id.id,
+            'partner_id': self.provider_id.id,
+            'partner_shipping_id': self.partner_shipping_id.id,
+            'fiscal_position_id': (self.fiscal_position_id or self.fiscal_position_id._get_fiscal_position(self.provider_id)).id,
+            'invoice_origin': self.name,
+            'invoice_payment_term_id': self.payment_term_id.id,
+            'invoice_user_id': self.user_id.id,
+            'payment_reference': self.reference,
+            'transaction_ids': [Command.set(txs_to_be_linked.ids)],
+            'company_id': self.company_id.id,
+            'invoice_line_ids': [],
+            'user_id': self.user_id.id,
+        }
+        if self.journal_id:
+            values['journal_id'] = self.journal_id.id
+        return values
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
